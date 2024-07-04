7685251c574cc702210781cb6f954e4e95a56d32
Garrett <garrettleegrahamanderson@gmail.com>
2024-07-03 21:45:24 -0400

    added third and final testimionial

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -27,9 +27,9 @@ function Testimonial() {
         "Jackie has been our trusted petsitter for 19 years. This relationship is grounded in Jackie's unwavering commitment to providing the highest levels of care for our pets and home. We use Jackie for daily dog walks, visits, and vacation stays, and the peace of mind is invaluable. If consistent and capable care for your pets is important to you, then there is no one better than Jackie!",
     },
     {
-      clientName: "Loren ipsum",
+      clientName: "F. A. Allen",
       testimonial:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ultricies at eros in eleifend.",
+        "The fact that Jackie has been caring for our cats for more than 15 years says it all. She's terrific! Jackie really gets to know both pets and pet parents -- our personalities, our preferences, our idiosyncrasies. In addition to providing superb kitty care at every visit, she's also a lot of fun and a great communicator. Our cats know Jackie and love having her visit; they welcome the play time, and lots of attention and affection! She gave expert care to our wonderful, calm older cats with health issues and meds, and now brings great energy to our new generation: the enjoyable chaos of two mischievous, everything-is-a-toy rescue kittens. It's a big responsibility to care for the pets we love and make sure the house is in good working order. We have trusted Jackie with both our pets and our home for many years and plan to for many more. Thank you, Jackie!",
     },
   ];
 

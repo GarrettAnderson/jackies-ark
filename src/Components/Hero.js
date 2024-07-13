96b0321f7cace48b74d0a424965223fa96ac5d42
Garrett <garrettleegrahamanderson@gmail.com>
2024-07-13 00:01:29 -0400

    working on design of hero image

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Hero(props) {
-  return <section className="hero-image--section-containers"></section>;
+  return (
+    <section className="hero-image-parent-container">
+      <section className="hero-image--section-containers"></section>
+    </section>
+  );
 }
 
 export default Hero;

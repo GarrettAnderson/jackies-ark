6b69f90f2f6fa56c0d159dffcc7a8d02155d45a5
Garrett <garrettleegrahamanderson@gmail.com>
2024-10-18 18:26:56 -0400

    added font awesome hamburger menu to header component

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,14 @@
 import "../Assets/Styles/Header.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Header() {
   return (
     <header className="header-container">
+      <nav className="header-mobile-nav">
+        <FontAwesomeIcon icon={faBars} />
+      </nav>
+
       <nav className="header-desktop-nav">
         <ol>
           <li>ABOUT</li>
